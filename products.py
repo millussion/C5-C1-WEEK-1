ecccc70c8dc959b582103914a5fed9396feb5f27
Camille Marrugo <marrugocamille@gmail.com>
2026-03-27 17:19:28 -0500

    Update products.py
    Added comments to improve code readability.

--- products.py
+++ products.py
@@ -1,9 +1,9 @@
-products = []
+products = [] #list
 
 def new_product():
     product_name = input("Enter product name: ")
     running = True
-    while running:
+    while running: #validate that only numbers are entered
         try:
             price = int(input("Enter the unit price of the product: ").replace(",","").replace(".",""))
             running = False
@@ -11,7 +11,7 @@ def new_product():
             print("wrong. just numbers")
 
     running = True
-    while running:
+    while running: #validate that only numbers are entered
         try:
             amount = int(input("Enter quantity: "))
             running = False
@@ -19,14 +19,14 @@ def new_product():
             print("wrong. just numbers")
 
     total = price * amount
-
+    #We save the info in a dictionary
     save_product = {
         "name": product_name,
         "price": price,
         "amount": amount,
         "total": total
     }
-
+    #here we are sending the dictionary to the list
     products.append(save_product)
 
 
